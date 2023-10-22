4aa527c7dea2e21a6e1c0906fe1bd167298fdf18
HallamSaunders <hals4072@gmail.com>
2023-10-22 17:34:29 +0100

    Added functionality to decrypt a file once it has been encrypted.

--- ConsoleHandler.java
+++ ConsoleHandler.java
@@ -2,6 +2,8 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -35,6 +37,7 @@ public class ConsoleHandler {
             byte[] keyBytes = new byte[16];
             random.nextBytes(keyBytes);
             SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+            System.out.println("Key is: " + Arrays.toString(keyBytes));
 
             //Generate encrypted file
             File inputFile = new File(filePath);
@@ -44,18 +47,18 @@ public class ConsoleHandler {
             //Save the encrypted file
             System.out.println("What would you like to call this file?");
             String fileName = scanner.nextLine();
-            SaveEncryptedFile(encryptedBytes, fileName, pruneFilePath(filePath));
+            SaveFile(encryptedBytes, fileName, pruneFilePath(filePath));
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void SaveEncryptedFile(byte[] encryptedFileByte, String fileName, String filePath) throws IOException {
-        File outputFile = new File(filePath + fileName);
-        FileOutputStream outputStream = new FileOutputStream(outputFile);
+    public static void SaveFile(byte[] encryptedFileByte, String fileName, String filePath) throws IOException {
+        File unencryptedFile = new File(filePath + fileName);
+        FileOutputStream outputStream = new FileOutputStream(unencryptedFile);
         outputStream.write(encryptedFileByte);
         outputStream.close();
-        System.out.println("File encrypted and saved as " + fileName);
+        System.out.println("File encrypted and saved as " + fileName + ", at: " + filePath);
     }
 
     public static String pruneFilePath(String filePath){
@@ -69,7 +72,33 @@ public class ConsoleHandler {
          return filePath;
     }
 
-    public static void Decrypt(){
+    public static void Decrypt() throws IOException {
+        System.out.println("Enter the file path of the file you wish to encrypt.");
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine();
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the key used to encrypt the file.");
+        String keyString = reader.readLine();
+        String[] keyStringArray = keyString.split(", ");
+        byte[] keyBytes = new byte[16];
+        for(int i = 0; i < keyBytes.length; i++){
+            int num = Integer.parseInt(keyStringArray[i]);
+            keyBytes[i] = (byte) num;
+        }
+
+        SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+        try {
+            File encryptedFile = new File(filePath);
+            byte[] decryptedBytes = EncryptDecrypt.decryptFileStream(encryptedFile, keyBytes);
+
+            //Save the decrypted file
+            System.out.println("What would you like to call this file?");
+            String fileName = scanner.nextLine();
+            SaveFile(decryptedBytes, fileName, pruneFilePath(filePath));
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -1,19 +1,42 @@
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class EncryptDecrypt {
+    private static final byte[] buffer = new byte[1024];
     public static byte[] encryptFileStream(InputStream inputStream, SecretKey secretKey) throws Exception {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
-        byte[] buffer = new byte[1024];
         int bytesRead;
         while ((bytesRead = inputStream.read(buffer)) != -1) {
             byte[] encryptedBytes = cipher.update(buffer, 0, bytesRead);
             outputStream.write(encryptedBytes);
+        }
+
+        byte[] finalBytes = cipher.doFinal();
+        outputStream.write(finalBytes);
+
+        return outputStream.toByteArray();
+    }
+
+    public static byte[] decryptFileStream(File inputFile, byte[] keyBytes) throws Exception {
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] decryptedBytes = cipher.update(buffer, 0, bytesRead);
+            outputStream.write(decryptedBytes);
         }
 
         byte[] finalBytes = cipher.doFinal();
